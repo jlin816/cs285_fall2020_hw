0665f366097f4ba1cc081c85d5b46cd9e96aff76
Jessy Lin <jessy81697@gmail.com>
2020-10-19 12:56:09 -0700

    Add scripts

---
+++ hw3/cs285/q2_dqn.sh
@@ -0,0 +1,3 @@
+python scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q2_dqn_1 --seed 1 & \
+python scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q2_dqn_2 --seed 2 & \
+python scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q2_dqn_3 --seed 3
---
+++ hw3/cs285/q3.sh
@@ -0,0 +1,3 @@
+python scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q3_hparam1 --target_update_freq 1000 & \
+python scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q3_hparam2 --target_update_freq 5000 & \
+python scripts/run_hw3_dqn.py --env_name LunarLander-v3 --exp_name q3_hparam3 --target_update_freq 7000 &
